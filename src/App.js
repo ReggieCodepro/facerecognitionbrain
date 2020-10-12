99e7d6a06dcac12af301bbd3fa2cea58a3121ede
yungengine8 <yungengine8@gmail.com>
2020-10-11 23:14:36 -0400

    video 14:30

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 class App extends Component { 
@@ -6,8 +8,10 @@ class App extends Component {
     return (
       <div className="App"> 
       <Navigation />
+      <Logo />
 
-      {/*<logo />
+      {/*
+      <ImageLinkForm />
       <FaceRecognition />*/}
       </div>
     )
