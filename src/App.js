185f74aefb1113aa6cc5857de9fae13197bfc459
Reginald Wright <yungengine8@gmail.com>
2020-10-13 03:05:01 -0400

    done

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   render(){
     return (
       <div className="App"> 
-      return (
+      
             <Particles className='particles'
               params={particlesOptions}
             />
