002f8aae4710057113e3871a6fd9d277a059745a
Reginald Wright <yungengine8@gmail.com>
2020-10-14 22:08:45 -0400

    Added Imagelink

--- src/App.js
+++ src/App.js
@@ -21,6 +21,18 @@ const particlesOptions = {
   }
 
 class App extends Component { 
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  onButtonSubmit = () => {
+    console.log('click');
+  }
   render(){
     return (
       <div className="App"> 
@@ -30,7 +42,7 @@ class App extends Component {
             />
       <Navigation />
       <Logo />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
       <Rank />
       
 
