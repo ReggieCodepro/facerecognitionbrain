3182a4cba10b43c074d35b29e7a26985c1092d60
yungengine8 <yungengine8@gmail.com>
2020-12-11 17:23:39 -0500

    where I left off its working

--- src/App.js
+++ src/App.js
@@ -13,32 +13,47 @@ import './App.css';
 const app = new Clarifai.App({
   apiKey: 'abcb33311e694dc1a570307d3f57561d'
 });
-
 const particlesOptions = {
-  
     particles: {
       number:{
-        value: 60,
+        value: 30,
         density: {
           enable: true,
           value_area: 800
         }
       }
-
     }
   }
-
 class App extends Component { 
   constructor(){
     super();
     this.state = {
       input: '',
       imageUrl:'',
-      box: {},
-      route: 'signin',
-      isSignedIn: 'false'
+      box: {}, 
+      route: 'signin', 
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '', 
+        email: '',
+        entries: 0,
+        joined: ''
+
+      }
     }
   }
+  loadUser = (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name, 
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+
+    }})
+  }
+
   calculateFaceLocation = (data) => {
     const clarifaiFace =data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
@@ -51,9 +66,7 @@ class App extends Component {
       bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   }
-
   displayFaceBox = (box) => {
-    
     this.setState({box: box});
   }
   onInputChange = (event) => {
@@ -65,9 +78,24 @@ class App extends Component {
     .predict(
       Clarifai.FACE_DETECT_MODEL,
      this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+      console.log('hi', response)
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err => console.log(err));
-    
   }
   onRouteChange = (route) => {
     if(route === 'signout'){
@@ -81,16 +109,17 @@ class App extends Component {
     const {isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App"> 
-      
             <Particles className='particles'
               params={particlesOptions}
             />
-      
       <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
       { route === 'home'
          ?<div>
              <Logo />
-             <Rank />
+             <Rank
+             name={this.state.user.name}
+             entries={this.state.user.entries}
+              />
              <ImageLinkForm
                onInputChange={this.onInputChange} 
                onButtonSubmit={this.onButtonSubmit}
@@ -99,15 +128,12 @@ class App extends Component {
             </div>
           :(
             route === 'signin' 
-            ?<Signin onRouteChange={this.onRouteChange} />
-            :<Register onRouteChange={this.onRouteChange} />
+            ?<Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            :<Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
-         
-         
       }
       </div>
     );
   }
 }
-
 export default App;
